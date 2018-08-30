f2c93aaa20d6d4e653f0ccffd46eb30a3348f94c
ticotaco72 <32620809+ticotaco72@users.noreply.github.com>
2018-08-30 17:03:38 +0200

    Start creating some platform classes for android

---
+++ osu.Framework/Platform/Android/AndroidStorage.cs
@@ -0,0 +1,44 @@
+﻿// Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu-framework/master/LICENCE
+
+#if XAMARINANDROID
+using Android.Content;
+#endif
+using System.IO;
+using osu.Framework.IO.File;
+using osu.Framework.Platform;
+using System.Collections.Generic;
+using System.Text;
+
+namespace osu.Framework.Platform.Android
+{
+    /*class AndroidStorage : Storage
+    {
+        public AndroidStorage(string baseName)
+            : base(baseName)
+        {
+        }
+
+        //Dopilnuj, aby zamieniać ścieżki względne na bezwzględne
+
+        protected override string LocateBasePath()
+        {
+            return Context.GetExternalFilesDir("private");
+        }
+
+        public override string[] GetFiles(string path)
+        {
+            return (string[])Directory.EnumerateFiles(path);
+        }
+
+        public override void Delete(string path)
+        {
+            FileSafety.FileDelete(GetUsablePathFor(path));
+        }
+
+        public override bool Exists(string path)
+        {
+            File.Exists(GetUsablePathFor(path));
+        }
+    }*/
+}
