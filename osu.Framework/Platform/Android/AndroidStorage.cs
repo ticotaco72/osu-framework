d93edaeab4418fa130ba7a3cb39c6040fbd3a52c
ticotaco72 <32620809+ticotaco72@users.noreply.github.com>
2018-08-31 20:33:10 +0200

    multitargetting stuff try

--- osu.Framework/Platform/Android/AndroidStorage.cs
+++ osu.Framework/Platform/Android/AndroidStorage.cs
@@ -1,7 +1,9 @@
 ﻿// Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu-framework/master/LICENCE
 
-//using Android.Content;
+#if XAMARINANDROID
+using Android.Content;
+#endif
 using System.IO;
 using osu.Framework.IO.File;
 using osu.Framework.Platform;
