a9143116cce864316120ad324c7976f8033d3c6f
ticotaco72 <32620809+ticotaco72@users.noreply.github.com>
2018-09-01 22:23:07 +0200

    Create good solution structure that'll be used forever (if not changed). work with androidstorage

---
+++ osu.Framework.Platform.Android/AndroidStorage.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using osu.Framework.Platform;
+using osu.Framework.IO.File;
+using System.IO;
+
+namespace osu.Framework.Platform.Android
+{
+    class AndroidStorage : Storage
+    {
+        public AndroidStorage(string baseName)
+            : base(baseName)
+        {
+        }
+
+        //Dopilnuj, aby zamieniać ścieżki względne na bezwzględne
+
+        protected override string LocateBasePath()
+        {
+            var context = Context;
+            return context.GetExternalFilesDir();
+        }
+
+        public override string[] GetFiles(string path)
+        {
+            return (string[])Directory.EnumerateFiles(path);
+        }
+
+        public override void Delete(string path)
+        {
+            FileSafety.FileDelete(GetUsablePathFor(path));
+        }
+
+        public override bool Exists(string path)
+        {
+            return File.Exists(GetUsablePathFor(path));
+        }
+    }
+}
