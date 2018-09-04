0665e9ee339bd1bb292222d6b9afc1e54114b3e3
ticotaco72 <32620809+ticotaco72@users.noreply.github.com>
2018-09-04 15:10:24 +0200

    work in progress

---
+++ osu.Framework.Platform.Android/AndroidClipboard.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using osu.Framework.Platform;
+
+namespace osu.Framework.Platform.Android
+{
+    class AndroidClipboard : Clipboard
+    {
+        public override string GetText()
+        {
+            ClipboardManager clipboard = (ClipboardManager) Application.Context.GetSystemService("clipboard");
+            return "";
+        }
+
+        public override void SetText(string selectedText)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
