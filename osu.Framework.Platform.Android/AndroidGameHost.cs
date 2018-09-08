5ad53119d408191bb9665588de5e33dff534725d
ticotaco72 <32620809+ticotaco72@users.noreply.github.com>
2018-09-08 21:18:39 +0200

    move bass library to android project, add androidgamehost and make some not very good manipulations on opentk

---
+++ osu.Framework.Platform.Android/AndroidGameHost.cs
@@ -0,0 +1,36 @@
+﻿extern alias PPY;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PPY::OpenTK;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using osu.Framework.Platform;
+
+namespace osu.Framework.Platform.Android
+{
+    class AndroidGameHost : DesktopGameHost
+    {
+        protected override Storage GetStorage(string baseName) => new AndroidStorage(baseName, this);
+
+        public override Clipboard GetClipboard() => new AndroidClipboard();
+
+        internal AndroidGameHost(string gameName, bool bindIPC = false)
+            : base(gameName, bindIPC)
+        {
+            Window = new AndroidGameWindow();
+            Window.WindowStateChanged += (sender, e) =>
+            {
+                if (Window.WindowState != PPY::OpenTK.WindowState.Minimized)
+                    OnActivated();
+                else
+                    OnDeactivated();
+            };
+        }
+    }
+}
