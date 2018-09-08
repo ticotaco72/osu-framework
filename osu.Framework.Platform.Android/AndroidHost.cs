a7b479827aebdc686400412fb925fb93c10a5bef
ticotaco72 <32620809+ticotaco72@users.noreply.github.com>
2018-09-08 22:24:37 +0200

    add better samplegame for android - start work add androidhost

---
+++ osu.Framework.Platform.Android/AndroidHost.cs
@@ -0,0 +1,13 @@
+﻿using osu.Framework.Platform;
+using osu.Framework.Platform.Android;
+
+namespace osu.Framework
+{
+    public static class AndroidHost
+    {
+        public static DesktopGameHost GetSuitableHost(string gameName, bool bindIPC = false)
+        {
+            return new AndroidGameHost(gameName, bindIPC);
+        }
+    }
+}
