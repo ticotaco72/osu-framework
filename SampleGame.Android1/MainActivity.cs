a2e8cdeff35a97afe9a9ccf716b69fb6896f54ca
ticotaco72 <32620809+ticotaco72@users.noreply.github.com>
2018-09-08 22:24:37 +0200

    add real samplegame and add androidhost class - it's used as host class

---
+++ SampleGame.Android1/MainActivity.cs
@@ -0,0 +1,25 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using System;
+using osu.Framework.Platform;
+using osu.Framework;
+
+namespace SampleGame
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+        }
+        protected override void OnStart()
+        {
+            base.OnStart();
+            using (Game game = new SampleGame())
+            using (GameHost host = AndroidHost.GetSuitableHost(@"sample-game"))
+            host.Run(game);
+        }
+    }
+}
