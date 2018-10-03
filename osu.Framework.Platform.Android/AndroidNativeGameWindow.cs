f558bacc9be233a1bcc1302cc4b3d5557ff5fe8e
ticotaco72 <32620809+ticotaco72@users.noreply.github.com>
2018-10-03 23:08:12 +0200

    start creating base sturcture of window in manual method  in framework not editing opentk

---
+++ osu.Framework.Platform.Android/AndroidNativeGameWindow.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using osuTK;
+using osuTK.Platform;
+
+namespace osu.Framework.Platform.Android
+{
+    public class AndroidNativeGameWindow : View, IGameWindow
+    {
+        public AndroidNativeGameWindow(Context context, IAttributeSet attrs) :
+            base(context, attrs)
+        {
+            Initialize();
+        }
+
+        public AndroidNativeGameWindow(Context context, IAttributeSet attrs, int defStyle) :
+            base(context, attrs, defStyle)
+        {
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+        }
+    }
+}
